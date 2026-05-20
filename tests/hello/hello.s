e8fc18c6b8b84956a43eba841f4e1f8abc732e41
TheGeniusOfEternity <max06safin@yandex.ru>
2026-05-20 18:13:01 +0300

    feat(translation|tests): hello

---
+++ tests/hello/hello.s
@@ -0,0 +1,22 @@
+; Hello World – prints "Hello, World!" then stops
+%define IN_PORT  0xFFFFFF00
+%define OUT_PORT 0xFFFFFF04
+
+.data
+msg:    pstr "Hello, World!"
+
+.code
+.org 0x1000
+start:
+    mv.l    #msg, R0         ; R0 = string address
+    mv.b    (R0)+, R1        ; R1 = length, R0 указывает на первый символ
+    mv.l    R1, R2           ; set R2 as amount of remaining symbols
+loop:
+    cmp.b   #0, R2           ; compare R2 with 0
+    beq     done             ; if R2 is 0 then goto done
+    mv.b    (R0)+, R1        ; load current symbol
+    mv.b    R1, (OUT_PORT)   ; output
+    sub.b   #1, R2           ; decrement R2
+    jmp     loop
+done:
+    die                      ; stop the program
